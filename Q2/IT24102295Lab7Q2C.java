687beaed94ab63a039a0ad3086c678dc26420b2c
IT24102295 <it24102295@my.sliit.lk>
2024-09-23 22:38:59 +0530

    IT24102295Lab7Q2C

---
+++ Q2/IT24102295Lab7Q2C.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class IT24102295Lab7QC {
+       public static void main(String[] args) {
+           Scanner input = new Scanner(System.in);
+           int i,j;
+           for( i=5;i>=1;i-- )
+           {
+              for( j=1;j<=i;j++ )
+              { 
+                System.out.print(i);
+              }
+           System.out.println();
+           }
+       }
+}  
